51ed54d9b231e914204476dd18a3d2dfd40630d4
robin2br <robinsonb223@gmail.com>
2026-02-04 19:11:29 -0500

    Profile/About Page

---
+++ UCTrafficApp/Pages/ReportIssueThanksPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace UCTrafficApp.Pages;
+
+public partial class ReportIssueThanksPage : ContentPage
+{
+    public ReportIssueThanksPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void Return_Clicked(object sender, EventArgs e)
+    {
+        // Pops back to where you came from (Settings/Profile flow)
+        await Shell.Current.GoToAsync("..");
+        await Shell.Current.GoToAsync("..");
+    }
+}
